82214588ecc813bddd4f4404008573faac8abdc1
McVITIES755 <117489915+McVITIES755@users.noreply.github.com>
2023-12-25 18:04:45 +0000

    Add files via upload

---
+++ AudioList.js
@@ -0,0 +1,104 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Save Your Tears (Remix)",
+    artist: "The Weeknd and Ariana Grande",
+    img: "images/Save-Your-Tears.jpg",
+    src: "Save-Your-Tears"
+  },
+  {
+    name: "Locked Out Of Heaven",
+    artist: "Bruno Mars",
+    img: "images/Locked-Out-Of-Heaven.jpg",
+    src: "Locked-Out-Of-Heaven"
+  },
+  {
+    name: "Cheap Thrills",
+    artist: "Sia and Sean Paul",
+    img: "images/Cheap-Thrills.jpg",
+    src: "Cheap-Thrills"
+  },
+  {
+    name: "Derniere Danse",
+    artist: "Indila",
+    img: "images/Derniere-Danse.jpg",
+    src: "Derniere-Danse"
+  },
+  {
+    name: "Tourner Dans Le Vide",
+    artist: "Indila",
+    img: "images/Tourner-Dans-Le-Vide.jpg",
+    src: "Tourner-Dans-Le-Vide"
+  },
+  {
+    name: "Paro",
+    artist: "Nej",
+    img: "images/Paro.jpg",
+    src: "Paro"
+  },
+  {
+    name: "Clean Up",
+    artist: "Chunkz and Yung Filly",
+    img: "images/Clean-Up.jpg",
+    src: "Clean-Up"
+  },
+  {
+    name: "Feel This Moment",
+    artist: "Pitbull and Christina Aguilera",
+    img: "images/Feel-This-Moment.jpg",
+    src: "Feel-This-Moment"
+  },
+  {
+    name: "Kick Down The Door",
+    artist: "Harris J and AJ Jung",
+    img: "images/Kick-Down-The-Door.jpg",
+    src: "Kick-Down-The-Door"
+  },
+  {
+    name: "Let Me Love You",
+    artist: "DJ Snake and Justin Beiber",
+    img: "images/Let-Me-Love-You.jpg",
+    src: "Let-Me-Love-You"
+  },
+  {
+    name: "One Dance",
+    artist: "Drake",
+    img: "images/One-Dance.jpg",
+    src: "One-Dance"
+  },
+  {
+    name: "Taki Taki",
+    artist: "DJ Snake Selena Gomez Ozuna & Cardi B",
+    img: "images/TakiTaki.jpg",
+    src: "TakiTaki"
+  },
+  {
+    name: "Unravel",
+    artist: "TK from 凛として時雨",
+    img: "images/Unravel.jpg",
+    src: "Unravel"
+  },
+  {
+    name: "Starboy",
+    artist: "Weeknd and Daft Punk",
+    img: "images/Starboy.jpg",
+    src: "Starboy"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+  // }
+];
